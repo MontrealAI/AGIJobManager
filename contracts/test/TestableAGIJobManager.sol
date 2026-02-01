6e9d9632c7798c8e40532b40c019c5d4a5294218
MONTREAL.AI <info@montreal.ai>
2026-01-31 20:46:54 -0500

    Enforce payout conservation constraints

---
+++ contracts/test/TestableAGIJobManager.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../AGIJobManager.sol";
+
+contract TestableAGIJobManager is AGIJobManager {
+    constructor(
+        address _agiTokenAddress,
+        string memory _baseIpfsUrl,
+        address _ensAddress,
+        address _nameWrapperAddress,
+        bytes32 _clubRootNode,
+        bytes32 _agentRootNode,
+        bytes32 _validatorMerkleRoot,
+        bytes32 _agentMerkleRoot
+    )
+        AGIJobManager(
+            _agiTokenAddress,
+            _baseIpfsUrl,
+            _ensAddress,
+            _nameWrapperAddress,
+            _clubRootNode,
+            _agentRootNode,
+            _validatorMerkleRoot,
+            _agentMerkleRoot
+        )
+    {}
+
+    function setValidationRewardPercentageUnsafe(uint256 _percentage) external onlyOwner {
+        validationRewardPercentage = _percentage;
+    }
+}
