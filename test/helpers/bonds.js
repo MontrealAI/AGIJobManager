3f43b6fb64c6459dc3fce12f466a75173ca75743
MONTREAL.AI <info@montreal.ai>
2026-02-04 22:16:24 -0500

    Reduce AGIJobManager bytecode size

--- test/helpers/bonds.js
+++ test/helpers/bonds.js
@@ -1,11 +1,23 @@
 async function fundValidators(token, manager, validators, owner, multiplier = 5) {
-  const bond = await manager.validatorBond();
-  const amount = bond.muln(multiplier);
+  const bondMax = await manager.validatorBondMax();
+  const amount = bondMax.muln(multiplier);
   for (const validator of validators) {
     await token.mint(validator, amount, { from: owner });
     await token.approve(manager.address, amount, { from: validator });
   }
+  return bondMax;
+}
+
+async function computeValidatorBond(manager, payout) {
+  const [bps, min, max] = await Promise.all([
+    manager.validatorBondBps(),
+    manager.validatorBondMin(),
+    manager.validatorBondMax(),
+  ]);
+  let bond = payout.mul(bps).divn(10000);
+  if (bond.lt(min)) bond = min;
+  if (bond.gt(max)) bond = max;
   return bond;
 }
 
-module.exports = { fundValidators };
+module.exports = { fundValidators, computeValidatorBond };
