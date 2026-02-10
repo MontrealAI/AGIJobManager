942703375e5fa508a4d5551f0e35a018cb8c12f4
MONTREAL.AI <info@montreal.ai>
2026-02-09 21:14:11 -0500

    Guard token update against stranded balances

--- contracts/test/MockNameWrapper.sol
+++ contracts/test/MockNameWrapper.sol
@@ -6,6 +6,11 @@ contract MockNameWrapper {
     mapping(address => mapping(address => bool)) private approvals;
     mapping(bytes32 => bool) private wrapped;
     mapping(bytes32 => uint32) private burnedFuses;
+    uint256 public setChildFusesCalls;
+    bytes32 public lastParentNode;
+    bytes32 public lastLabelhash;
+    uint32 public lastFuses;
+    uint64 public lastExpiry;
 
     function setOwner(uint256 id, address owner) external {
         owners[id] = owner;
@@ -31,6 +36,14 @@ contract MockNameWrapper {
         uint32 nextFuses = burnedFuses[node] | fuses;
         burnedFuses[node] = nextFuses;
         return nextFuses;
+    }
+
+    function setChildFuses(bytes32 parentNode, bytes32 labelhash, uint32 fuses, uint64 expiry) external {
+        setChildFusesCalls += 1;
+        lastParentNode = parentNode;
+        lastLabelhash = labelhash;
+        lastFuses = fuses;
+        lastExpiry = expiry;
     }
 
     function setSubnodeRecord(
