5028b0760913e196823be2088a24cfb9b26204d5
MONTREAL.AI <info@montreal.ai>
2026-02-15 11:30:07 -0500

    Harden ENS integration ABI checks and operational docs

--- contracts/ens/INameWrapper.sol
+++ contracts/ens/INameWrapper.sol
@@ -5,7 +5,6 @@ interface INameWrapper {
     function ownerOf(uint256 id) external view returns (address);
     function isApprovedForAll(address owner, address operator) external view returns (bool);
     function getApproved(uint256 tokenId) external view returns (address);
-    function isWrapped(bytes32 node) external view returns (bool);
     function setChildFuses(bytes32 parentNode, bytes32 labelhash, uint32 fuses, uint64 expiry) external;
     function setSubnodeRecord(
         bytes32 parentNode,
